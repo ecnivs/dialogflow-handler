56db30a31ffdbb88b70c8d36a1a94502ebf6c4cd
Vince Swu <vinceswu88@gmail.com>
2024-11-30 03:04:18 +0530

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dialogflow-handler',
-    version='0.1.0',
+    version='0.1.1',
     description='Another Dialogflow API wrapper',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
